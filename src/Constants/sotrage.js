0a4a7b143b4795714e184a64f214fb2fb5d6d34d
Bilal sheikh <bilalsheikhsb77@gmail.com>
2026-04-19 09:44:33 +0500

    All works are done almost

---
+++ src/Constants/sotrage.js
@@ -0,0 +1,8 @@
+// ✅ 100% working fix — storage.js alag file banao
+const storage = {
+    getItem: (key) => Promise.resolve(window.localStorage.getItem(key)),
+    setItem: (key, value) => Promise.resolve(window.localStorage.setItem(key, value)),
+    removeItem: (key) => Promise.resolve(window.localStorage.removeItem(key)),
+}
+
+export default storage
