057cfb1928fdd1f275af1932b9d62deb67cd175f
Bilal sheikh <bilalsheikhsb77@gmail.com>
2026-04-21 11:23:08 +0500

    Url keys update

--- src/Constants/keys.js
+++ src/Constants/keys.js
@@ -1,4 +1,4 @@
-export const Secret_api_key = "https://jobportalbackend-production-158e.up.railway.app/"
+export const Secret_api_key = "https://jobportalbackend-production-158e.up.railway.app"
 export const Secret_admin_keys = "https://jobportalbackend-production-158e.up.railway.app/company/"
 export const Secret_admin_Jobs_keys = "https://jobportalbackend-production-158e.up.railway.app/job/"
 export const Secret_admin_posts_keys = "https://jobportalbackend-production-158e.up.railway.app/post"
